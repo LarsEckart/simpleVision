e9a9ceb6b9c63a436795081ff9a4e516061bc754
Lars Eckart <lars.eckart@hey.com>
2024-03-26 20:36:44 +0200

    migrate mapper tests to use the new class

--- simple-vision-core/src/test/java/com/simplevision/core/mapper/PrescriptionMapperTest.java
+++ simple-vision-core/src/test/java/com/simplevision/core/mapper/PrescriptionMapperTest.java
@@ -1,6 +1,6 @@
 package com.simplevision.core.mapper;
 
-import com.simplevision.core.domain.Info;
+import com.simplevision.core.domain.Prescription;
 import com.simplevision.core.view.PrescriptionDetails;
 import com.simplevision.core.view.PrescriptionView;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,134 +10,134 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PrescriptionMapperTest {
     private PrescriptionMapper mapper;
-    private Info info;
+    private Prescription prescription;
 
-    private PrescriptionView prescription;
+    private PrescriptionView prescriptionView;
     private PrescriptionDetails od;
     private PrescriptionDetails os;
 
     @BeforeEach
     public void setup() {
         mapper = new PrescriptionMapper();
-        info = new Info();
+        prescription = new Prescription();
 
-        prescription = new PrescriptionView();
+        prescriptionView = new PrescriptionView();
         od = new PrescriptionDetails();
         os = new PrescriptionDetails();
 
-        prescription.setOS(os);
-        prescription.setOD(od);
+        prescriptionView.setOS(os);
+        prescriptionView.setOD(od);
     }
 
     @Test
     public void setId() {
-        info.setId(1);
+        prescription.setId(1);
 
-        assertEquals(1, mapper.map(info).getId());
+        assertEquals(1, mapper.map(prescription).getId());
     }
 
     @Test
     public void setPD() {
-        info.setPD("pd");
+        prescription.setPD("pd");
 
-        assertEquals("pd", mapper.map(info).getPd());
+        assertEquals("pd", mapper.map(prescription).getPd());
     }
 
     @Test
     public void setLeftAxis() {
-        info.setLeftAxis("leftAxis");
+        prescription.setLeftAxis("leftAxis");
 
-        assertEquals("leftAxis", mapper.map(info).getOS().getAxis());
+        assertEquals("leftAxis", mapper.map(prescription).getOS().getAxis());
     }
 
     @Test
     public void setLeftCylinder() {
-        info.setLeftCylinder("leftCylinder");
+        prescription.setLeftCylinder("leftCylinder");
 
-        assertEquals("leftCylinder", mapper.map(info).getOS().getCylinder());
+        assertEquals("leftCylinder", mapper.map(prescription).getOS().getCylinder());
     }
 
     @Test
     public void setLeftSphere() {
-        info.setLeftSphere("leftSphere");
+        prescription.setLeftSphere("leftSphere");
 
-        assertEquals("leftSphere", mapper.map(info).getOS().getSphere());
+        assertEquals("leftSphere", mapper.map(prescription).getOS().getSphere());
     }
 
     @Test
     public void setRightAxis() {
-        info.setRightAxis("rightAxis");
+        prescription.setRightAxis("rightAxis");
 
-        assertEquals("rightAxis", mapper.map(info).getOD().getAxis());
+        assertEquals("rightAxis", mapper.map(prescription).getOD().getAxis());
     }
 
     @Test
     public void setRightCylinder() {
-        info.setRightCylinder("rightCylinder");
+        prescription.setRightCylinder("rightCylinder");
 
-        assertEquals("rightCylinder", mapper.map(info).getOD().getCylinder());
+        assertEquals("rightCylinder", mapper.map(prescription).getOD().getCylinder());
     }
 
     @Test
     public void setRightSphere() {
-        info.setRightSphere("rightSphere");
+        prescription.setRightSphere("rightSphere");
 
-        assertEquals("rightSphere", mapper.map(info).getOD().getSphere());
+        assertEquals("rightSphere", mapper.map(prescription).getOD().getSphere());
     }
 
     @Test
     public void mapInfo_rightSphere() {
         od.setSphere("rightSphere");
 
-        assertEquals("rightSphere", mapper.map(prescription).getRightSphere());
+        assertEquals("rightSphere", mapper.map(prescriptionView).getRightSphere());
     }
 
     @Test
     public void mapInfo_rightCylinder() {
         od.setCylinder("rightCylinder");
 
-        assertEquals("rightCylinder", mapper.map(prescription).getRightCylinder());
+        assertEquals("rightCylinder", mapper.map(prescriptionView).getRightCylinder());
     }
 
     @Test
     public void mapInfo_rightAxis() {
         od.setAxis("rightAxis");
 
-        assertEquals("rightAxis", mapper.map(prescription).getRightAxis());
+        assertEquals("rightAxis", mapper.map(prescriptionView).getRightAxis());
     }
 
     @Test
     public void mapInfo_leftSphere() {
         os.setSphere("leftSphere");
 
-        assertEquals("leftSphere", mapper.map(prescription).getLeftSphere());
+        assertEquals("leftSphere", mapper.map(prescriptionView).getLeftSphere());
     }
 
     @Test
     public void mapInfo_leftAxis() {
         os.setAxis("leftAxis");
 
-        assertEquals("leftAxis", mapper.map(prescription).getLeftAxis());
+        assertEquals("leftAxis", mapper.map(prescriptionView).getLeftAxis());
     }
 
     @Test
     public void mapInfo_leftCylinder() {
         os.setCylinder("leftCylinder");
 
-        assertEquals("leftCylinder", mapper.map(prescription).getLeftCylinder());
+        assertEquals("leftCylinder", mapper.map(prescriptionView).getLeftCylinder());
     }
 
     @Test
     public void mapInfo_pd() {
-        prescription.setPd("pd");
+        prescriptionView.setPd("pd");
 
-        assertEquals("pd", mapper.map(prescription).getPD());
+        assertEquals("pd", mapper.map(prescriptionView).getPD());
     }
 
     @Test
     public void mapInfo_id() {
-        prescription.setId(1);
+        prescriptionView.setId(1);
 
-        assertEquals(1, mapper.map(prescription).getId());
+        assertEquals(1, mapper.map(prescriptionView).getId());
     }
 }
